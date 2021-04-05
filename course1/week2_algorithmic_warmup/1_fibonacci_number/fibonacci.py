8c43b03e9f17d2c042154c2a0c307c735d31d44b
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2021-04-05 12:04:17 +0200

    week2 asignement problem 1

--- course1/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ course1/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,9 +1,21 @@
 # Uses python3
 def calc_fib(n):
-    if (n <= 1):
+    if n <= 1:
         return n
 
     return calc_fib(n - 1) + calc_fib(n - 2)
 
-n = int(input())
-print(calc_fib(n))
+
+def calc_fib_optimized(n):
+    f = []
+    f.append(0)
+    f.append(1)
+
+    for i in range(2, n + 1):
+        f.append(f[i - 1] + f[i - 2])
+    return f[n]
+
+
+if __name__ == "__main__":
+    n = int(input())
+    print(calc_fib_optimized(n))
