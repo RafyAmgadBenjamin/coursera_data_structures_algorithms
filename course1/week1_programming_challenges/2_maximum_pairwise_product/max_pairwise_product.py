8c894076d3243edbc143fa6053060b4bb60f1cdf
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2021-04-05 12:04:01 +0200

    week 1 assignement

--- course1/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ course1/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -6,13 +6,27 @@ def max_pairwise_product(numbers):
     max_product = 0
     for first in range(n):
         for second in range(first + 1, n):
-            max_product = max(max_product,
-                numbers[first] * numbers[second])
+            max_product = max(max_product, numbers[first] * numbers[second])
 
     return max_product
 
 
-if __name__ == '__main__':
+def max_pairwise_product_fast(numbers):
+    max_no1 = -9999999999
+    max_no1_index = -1
+    max_no2 = -9999999999
+    for i in range(len(numbers)):
+        if numbers[i] > max_no1:
+            max_no1 = numbers[i]
+            max_no1_index = i
+
+    for i in range(len(numbers)):
+        if numbers[i] > max_no2 and i != max_no1_index:
+            max_no2 = numbers[i]
+    return max_no1 * max_no2
+
+
+if __name__ == "__main__":
     input_n = int(input())
     input_numbers = [int(x) for x in input().split()]
-    print(max_pairwise_product(input_numbers))
+    print(max_pairwise_product_fast(input_numbers))
