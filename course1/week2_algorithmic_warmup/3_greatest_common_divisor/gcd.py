5b205b6d26665464d57f030db2ec4e968aab5405
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2021-04-07 07:42:30 +0200

    working on week2 assignment

--- course1/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
+++ course1/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
@@ -1,6 +1,7 @@
 # Uses python3
 import sys
 
+
 def gcd_naive(a, b):
     current_gcd = 1
     for d in range(2, min(a, b) + 1):
@@ -10,7 +11,15 @@ def gcd_naive(a, b):
 
     return current_gcd
 
+
+def gdc_optimitzed(a, b):
+    if b == 0:
+        return a
+    a_prim = a % b
+    return gdc_optimitzed(b, a_prim)
+
+
 if __name__ == "__main__":
-    input = sys.stdin.read()
-    a, b = map(int, input.split())
-    print(gcd_naive(a, b))
+    # input = sys.stdin.read()
+    a, b = map(int, input().split())
+    print(gdc_optimitzed(a, b))
