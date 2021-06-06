cb821106f7bc3a3cfc166390385aa96b217d45fa
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2021-06-06 19:46:06 +0200

    working on assignments

--- course1/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
+++ course1/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
@@ -1,15 +1,28 @@
 # Uses python3
 import sys
 
+
 def optimal_weight(W, w):
     # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+    w = [0] + w
+    items = len(w)
+    capacity = W + 1
 
-if __name__ == '__main__':
+    # Create a weight matrix and write in initial values.
+    mat = [[0 for _ in range(items)] for _ in range(capacity)]
+
+    for i in range(1, items):
+        for j in range(1, capacity):
+            mat[j][i] = mat[j][i - 1]
+            if w[i] <= j:
+                val = mat[j - w[i]][i - 1] + w[i]
+                if mat[j][i] < val:
+                    mat[j][i] = val
+
+    return mat[-1][-1]
+
+
+if __name__ == "__main__":
     input = sys.stdin.read()
     W, n, *w = list(map(int, input.split()))
     print(optimal_weight(W, w))
--- course1/week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
+++ course1/week6_dynamic_programming2/2_partitioning_souvenirs/partition3.py
@@ -1,20 +1,79 @@
-# Uses python3
-import sys
-import itertools
+# # Uses python3
+# import sys
+# import itertools
+import numpy
 
-def partition3(A):
-    for c in itertools.product(range(3), repeat=len(A)):
-        sums = [None] * 3
-        for i in range(3):
-            sums[i] = sum(A[k] for k in range(len(A)) if c[k] == i)
 
-        if sums[0] == sums[1] and sums[1] == sums[2]:
-            return 1
+# def partition3(A):
+#     for c in itertools.product(range(3), repeat=len(A)):
+#         sums = [None] * 3
+#         for i in range(3):
+#             sums[i] = sum(A[k] for k in range(len(A)) if c[k] == i)
 
-    return 0
+#         if sums[0] == sums[1] and sums[1] == sums[2]:
+#             return 1
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *A = list(map(int, input.split()))
-    print(partition3(A))
+#     return 0
+
+
+# def partitions(W, n, items):
+#     """ Finds if number of partitions having capacity W is >=3
+#     (int, int, list) -> (int) """
+#     count = 0
+#     val = numpy.zeros((W + 1, n + 1))
+#     for i in range(1, W + 1):
+#         for j in range(1, n + 1):
+#             val[i][j] = val[i][j - 1]
+#             if items[j - 1] <= i:
+#                 temp = val[i - items[j - 1]][j - 1] + items[j - 1]
+#                 if temp > val[i][j]:
+#                     val[i][j] = temp
+#             if val[i][j] == W:
+#                 count += 1
+
+#     if count < 3:
+#         print("0")
+#     else:
+#         print("1")
+
+
+# if __name__ == "__main__":
+#     input = sys.stdin.read()
+#     n, *A = list(map(int, input.split()))
+#     # print(partition3(A))
+#     print(partitions(A))
+
+
+# Discrete Knapsack problem without repetition
+def partitions(W, n, items):
+    """ Finds if number of partitions having capacity W is >=3
+    (int, int, list) -> (int) """
+    count = 0
+    val = numpy.zeros((W + 1, n + 1))
+    for i in range(1, W + 1):
+        for j in range(1, n + 1):
+            val[i][j] = val[i][j - 1]
+            if items[j - 1] <= i:
+                temp = val[i - items[j - 1]][j - 1] + items[j - 1]
+                if temp > val[i][j]:
+                    val[i][j] = temp
+            if val[i][j] == W:
+                count += 1
+
+    if count < 3:
+        print("0")
+    else:
+        print("1")
+
+
+if __name__ == "__main__":
+    n = int(input())
+    item_weights = [int(i) for i in input().split()]
+    total_weight = sum(item_weights)
+    if n < 3:
+        print("0")
+    elif total_weight % 3 != 0:
+        print("0")
+    else:
+        partitions(total_weight // 3, n, item_weights)
 
